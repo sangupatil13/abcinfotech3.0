10a9c0bc5a09392d761341693ec019d4e53f28b4
Sangameshwar <bdrsangu@gmail.com>
2021-10-31 17:20:02 +0000

    files added by Sangamedshwar

---
+++ file1.java
@@ -0,0 +1,9 @@
+// This is simple java project with single file
+public class file1
+ {
+		public static void main(String args[])
+		{
+			System.out.println (" This is java project with file");
+		}
+ }
+
